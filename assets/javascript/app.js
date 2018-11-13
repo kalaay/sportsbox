23890f4abf9ee6483d63c2500a8eddc0763bf619
kalaay waaye <kalaay1111@gmail.com>
2018-11-13 02:29:37 -0500

    late night update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,7 +65,7 @@ $(function () {
   setTimeout(imageScreen("nba", news), 1001);
   // scoreboard=============================================================
   function scoreScreen(api, league) {
-    j = 6;
+    j = 4;
     var password = "tanakan13";
     var sportURL =
       "https://api.mysportsfeeds.com/v1.0/pull/" +
@@ -85,15 +85,15 @@ $(function () {
     }).then(function (response) {
       for (var i = 0; i < j; i++) {
         var score =
-          "<div class='col-6' id='block'><div class='d-flex'><div id='date'>" +
+          "<div class='col-2' id='block'><div class='d-flex'><div id='date'>" +
           moment(response.scoreboard.gameScore[i].game.date).format("MMM Do") +
-          "</div></div><div class='d-flex'><div>" +
+          "</div></div><div class='d-flex'>AWAY:&nbsp&nbsp<div>" +
           response.scoreboard.gameScore[i].game.awayTeam.Abbreviation +
-          "</div><div>" +
+          "</div><div>&nbsp" +
           response.scoreboard.gameScore[i].awayScore +
-          "</div></div><div class='d-flex'><div>" +
+          "</div></div><div class='d-flex'>HOME:&nbsp&nbsp<div>" +
           response.scoreboard.gameScore[i].game.homeTeam.Abbreviation +
-          "</div><div>" +
+          "&nbsp</div><div>&nbsp&nbsp" +
           response.scoreboard.gameScore[i].homeScore +
           "</div></div></div>";
 
